0c8eec8b5ba450123ad2b46c138694bd014b858a
Okami Hiro <yy20soccer@gmail.com>
2023-09-07 13:31:03 +0900

    みす

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,42 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-
-	const lessonStart = (les_number) {
-		fetch('/lesson-test?lesson_number', {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(newTask),
-		})
-			.then(response => {
-				if (response.ok) {
-					window.location.replace('/todo');
-				}
-			})
-			.catch(error => console.error('Error:', error));
-	}
-
-	return (
-		<div className="App">
-			<div class="logo-area border">
-			</div>
-			<div class="select-lesson-area">
-				<span>Let's Playing...</span>
-				<ol class="lessons">
-					<li class="lesson">
-						<button onClick={lessonStart(1)}>LESSON1</button>
-					</li>
-				</ol>
-			</div>
-			<div>
-				<button>設定</button>
-				<button>使い方</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
