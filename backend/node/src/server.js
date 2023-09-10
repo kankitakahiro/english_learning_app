fdf9366df4731577381ed84319005d7b44f288ba
Okami Hiro <yy20soccer@gmail.com>
2023-09-10 23:05:02 +0900

    テスト画像を追加しました

--- backend/node/src/server.js
+++ backend/node/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const path = require('path');
+const fs = require('fs');
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -23,27 +24,75 @@ app.get('/lesson-test', (req, res) => {
     const lesson = req.query.lesson;
     const number = req.query.number;
 
-    if (lesson === undefined || number === undefined) {
-        res.status(400).send('Bad Request');
-        return;
-    }
+    // if (lesson === undefined || number === undefined) {
+    //     res.status(400).send('Bad Request');
+    //     return;
+    // }
 
     // mysqlからデータを取得する
     // *****************
 
     // *****************
 
-    res.header('Access-Control-Allow-Origin', '*');
+    // 現在のディレクトリの位置をconsole.logで確認
+    // console.log(__dirname);
+    // fs.readdir('./', (err, files) => {
+    //     if (err) {
+    //         console.log(err);
+    //         res.status(500).send('Internal Server Error 10');
+    //         return;
+    //     }
+    //     console.log('-----------');
+    //     console.log(files);
+    //     console.log('-----------');
+    // });
+    
+    
+    // const data_path = "/Users/yamamotoyuta/Desktop/Hack U/code/english_learning_app/backend/node/b64_data";
 
-    // base64形式で返す
-    res.json({
-        "id": 1,
-        "answer": "hogehoge",
-        "wronge1": "apple",
-        "wronge2": "apple",
-        "wronge3": "apple",
-        "image": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAYAAACtWK6eAAABp0lEQVR4nO3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-    })
+    fs.readdir('./b64_data', (err, files) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error 1');
+            return;
+        }
+
+        
+        let image_data;
+        // .txt ファイルだけをフィルタリング
+        const textFiles = files.filter(file => path.extname(file) === '.text');
+        
+        // ランダムな .txt ファイルを選ぶ
+        const randomFile = textFiles[Math.floor(Math.random() * textFiles.length)];
+        
+
+        // ファイルの内容を読み取る
+        
+        fs.readFile(path.join('./b64_data', randomFile), 'utf8', (err, data) => {
+            if (err) {
+                res.status(500).send('Internal Server Error 2');
+                return;
+            }
+            image_data = data;
+
+            // ans = 'apple';
+            // item_list = ['apple', 'banana', 'peach', 'grape'] // シャッフル
+
+            // console.log(image_data);
+            // base64形式で返す
+            res.header('Access-Control-Allow-Origin', '*');
+            res.json({
+                "id": 1,
+                "answer": "hogehoge",
+                "wronge1": "apple",
+                "wronge2": "apple",
+                "wronge3": "apple",
+                "image": "data:image/png;base64"+image_data,
+            });
+        });
+    });
+
+    
 });
 
 app.listen(PORT, () => {
