b708bd23d0fa8178bacfd758489017d127bb1545
KANKI\kanki <kanki66781712@gmail.com>
2023-09-11 22:05:40 +0900

    databaseの接続設定の変更

--- backend/node/src/server.js
+++ backend/node/src/server.js
@@ -34,7 +34,6 @@ app.get('/lesson-test', (req, res) => {
 // PostgreSQL connect info
 const mysql = require('mysql');
 const dbConfig = {
-    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
