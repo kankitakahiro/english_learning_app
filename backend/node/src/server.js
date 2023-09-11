a42809e2ce8c30d89f9167063e77c9cf2e37e783
KANKI\kanki <kanki66781712@gmail.com>
2023-09-11 20:26:27 +0900

    databaseの設定

--- backend/node/src/server.js
+++ backend/node/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const path = require('path');
+const fs = require('fs');
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -30,20 +31,102 @@ app.get('/lesson-test', (req, res) => {
 
     // mysqlからデータを取得する
     // *****************
+// PostgreSQL connect info
+const mysql = require('mysql');
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+};
+// ルートハンドラーの定義
+app.get('/', (req, res) => {
+    // Cloud SQL データベースに接続
+    const connection = mysql.createConnection(dbConfig);
 
+    // 接続を開始
+    connection.connect((err) => {
+        if (err) {
+            console.error('データベースへの接続エラー:', err);
+            res.status(500).send('データベースへの接続エラー');
+            return;
+        }
+
+        // クエリを実行
+        connection.query('SELECT * FROM question', (error, results, fields) => {
+            // クエリ実行後、接続を終了
+            connection.end();
+
+            if (error) {
+                console.error('クエリエラー:', error);
+                res.status(500).send('クエリエラー');
+                return;
+            }
+
+            // クエリの結果をレスポンスとして返す
+            res.json(results);
+        });
+    });
+});
     // *****************
 
-    res.header('Access-Control-Allow-Origin', '*');
+    // 現在のディレクトリの位置をconsole.logで確認
+    // console.log(__dirname);
+    // fs.readdir('./', (err, files) => {
+    //     if (err) {
+    //         console.log(err);
+    //         res.status(500).send('Internal Server Error 10');
+    //         return;
+    //     }
+    //     console.log('-----------');
+    //     console.log(files);
+    //     console.log('-----------');
+    // });
+    
+    
+    // const data_path = "/Users/yamamotoyuta/Desktop/Hack U/code/english_learning_app/backend/node/b64_data";
 
-    // base64形式で返す
-    res.json({
-        "id": 1,
-        "answer": "hogehoge",
-        "wronge1": "apple",
-        "wronge2": "apple",
-        "wronge3": "apple",
-        "image": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAYAAACtWK6eAAABp0lEQVR4nO3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-    })
+    fs.readdir('./b64_data', (err, files) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error 1');
+            return;
+        }
+
+        
+        let image_data;
+        // .txt ファイルだけをフィルタリング
+        const textFiles = files.filter(file => path.extname(file) === '.text');
+        
+        // ランダムな .txt ファイルを選ぶ
+        const randomFile = textFiles[Math.floor(Math.random() * textFiles.length)];
+        
+
+        // ファイルの内容を読み取る
+        
+        fs.readFile(path.join('./b64_data', randomFile), 'utf8', (err, data) => {
+            if (err) {
+                res.status(500).send('Internal Server Error 2');
+                return;
+            }
+            image_data = data;
+
+            // ans = 'apple';
+            // item_list = ['apple', 'banana', 'peach', 'grape'] // シャッフル
+
+            // console.log(image_data);
+            // base64形式で返す
+            res.header('Access-Control-Allow-Origin', '*');
+            res.json({
+                "id": 1,
+                "answer": "hogehoge",
+                "wronge1": "apple",
+                "wronge2": "apple",
+                "wronge3": "apple",
+                "image": "data:image/png;base64,"+image_data,
+            });
+        });
+    });
 });
 
 app.listen(PORT, () => {
---
+++ frontend/react-app/src/components/Home.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+export default function Home() {
+    const lessonList = [
+        { title: 'LESOON1', id: 1 },
+        { title: 'LESSON2', id: 2 },
+        { title: 'LESSON3', id: 3 },
+        { title: 'LESSON4', id: 4 },
+        { title: 'LESSON5', id: 5 },
+    ];
+
+    const lessons = lessonList.map(lesson =>
+        < li key={lesson.id}>
+            <Link to={`/lesson/${lesson.id}/1`} className='lesson'>
+                <span>{lesson.title}</span>
+                <button className='btn-s'>PLAY</button>
+            </Link>
+        </li >
+
+    );
+
+    return (
+        <>
+            <main>
+                <div className="logo-area">
+                </div>
+                <div className="select-lesson-area">
+                    <span>Let's Play...</span>
+                    <ul className='lessons'>{lessons}</ul>
+                </div>
+                <div className='footer-d'>
+                    <div className='low-buttons'>
+                        <Link to="/lesson/1/result" className='btn-p'>Settings</Link>
+                        <Link to="/" className='btn-p'>Guide</Link>
+                    </div>
+                </div>
+            </main>
+        </>
+    );
+}
+
