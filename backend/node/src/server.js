7daf4e86dc755af4c17e06e06385c99230ee7226
KANKI\kanki <kanki66781712@gmail.com>
2023-09-11 22:05:06 +0900

    databaseの接続設定の変更

--- backend/node/src/server.js
+++ backend/node/src/server.js
@@ -37,7 +37,8 @@ const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    socketPath: process.env.INSTANCE_UNIX_SOCKET,
 };
 // ルートハンドラーの定義
 app.get('/mysql', (req, res) => {
