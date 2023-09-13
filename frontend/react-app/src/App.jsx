31b9b35d9a39be92994154b71ee2c920498dde5e
toumakido <ktcity14@gmail.com>
2023-09-13 20:59:29 +0900

    コンフリクト解消

--- frontend/react-app/src/App.jsx
+++ frontend/react-app/src/App.jsx
@@ -4,13 +4,10 @@ import Home from './components/Home';
 import Ilesson from './components/Ilesson';
 import Tlesson from './components/Tlesson';
 import Result from './components/Result';
-<<<<<<< HEAD
 import Login from './components/Login';
 import History from './components/History';
-=======
 import { LoginForm } from './components/LoginForm';
 import './style.css';
->>>>>>> origin/main
 
 export default function App() {
 	return (
@@ -20,12 +17,9 @@ export default function App() {
 				<Route path="/tlesson/:id/:number" element={<Tlesson />} />
 				<Route path="/ilesson/:id/:number" element={<Ilesson />} />
 				<Route path="/lesson/:id/result/:score" element={<Result />} />
-<<<<<<< HEAD
-				<Route path="/login" element={<Login />} />
+				{/* <Route path="/login" element={<Login />} /> */}
 				<Route path="/history" element={<History />} />
-=======
 				<Route path="/login" element={<LoginForm />} />
->>>>>>> origin/main
 			</Routes>
 		</Router>
 	);
