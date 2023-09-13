538f733e6e67e48f3ec91cfdcf7447024137ae72
toumakido <ktcity14@gmail.com>
2023-09-13 14:57:25 +0900

    フロントのログイン機能を作り始めた

--- frontend/react-app/src/components/History.jsx
+++ frontend/react-app/src/components/History.jsx
@@ -53,6 +53,7 @@ export default function Home() {
     );
     return (
         <>
+            <header></header>
             <ul>{lessons}</ul>
         </>
     );
--- frontend/react-app/src/components/Login.jsx
+++ frontend/react-app/src/components/Login.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link, useParams, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { auth } from '../firebase';
 
 
 export default function Login() {
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const { email, password } = event.target.elements;
+        auth.createUserWithEmailAndPassword(email.value, password.value);
+    };
+
     return (
         <>
             <header></header>
             <main className='login'>
-                <form className='login-form' action="http://localhost:8080" method='POST'>
+                <form className='login-form' onSubmit={handleLogin}>
                     <div className='form-item'>
                         <label htmlFor="email">Email</label>
                         <input type="email" className='form-c' name="email" id="email" />
@@ -19,7 +26,7 @@ export default function Login() {
                     </div>
                     <div className='low-buttons'>
                         <Link to="/" className='btn-s'>HOME</Link>
-                        <input className='btn-p' type="submit" />
+                        <button className='btn-p'>Login</button>
                     </div>
                 </form>
             </main >
