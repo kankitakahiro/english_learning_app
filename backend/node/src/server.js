e0b235111b7009a98f85b37f95adb2e6df0253bb
KANKI\kanki <kanki66781712@gmail.com>
2023-09-14 21:12:15 +0900

    データの挿入と問題の読み取りを作成

--- backend/node/src/server.js
+++ backend/node/src/server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const fs = require('fs');
 const admin = require('firebase-admin');
 const dotenv = require('dotenv');
+const mysql = require('mysql');
 if (process.env.NODE_ENV !== "production") { // production is 本番環境
     dotenv.config();
     console.log("development");
@@ -12,25 +13,25 @@ if (process.env.NODE_ENV !== "production") { // production is 本番環境
     console.log("production");
 }
 
-// const serviceAccount = {
-//     "type": process.env.apiKey,
-//     "project_id": process.env.project_id,
-//     "private_key_id": process.env.private_key_id,
-//     "private_key": process.env.private_key.replace(/\\n/g, '\n'),
-//     "client_email": process.env.client_email,
-//     "client_id": process.env.client_id,
-//     "auth_uri": process.env.auth_uri,
-//     "token_uri": process.env.token_uri,
-//     "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
-//     "client_x509_cert_url": process.env.client_x509_cert_url,
-//     "universe_domain": process.env.universe_domain
-// }
+const serviceAccount = {
+    "type": process.env.apiKey,
+    "project_id": process.env.project_id,
+    "private_key_id": process.env.private_key_id,
+    "private_key": process.env.private_key.replace(/\\n/g, '\n'),
+    "client_email": process.env.client_email,
+    "client_id": process.env.client_id,
+    "auth_uri": process.env.auth_uri,
+    "token_uri": process.env.token_uri,
+    "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+    "client_x509_cert_url": process.env.client_x509_cert_url,
+    "universe_domain": process.env.universe_domain
+}
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-// });
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
 
 app.use(express.json());
 
@@ -39,6 +40,30 @@ app.get('/test', (req, res) => {
 });
 
 // function mySql
+let pool;
+if (process.env.NODE_ENV !== "production") { // production is 本番環境
+    dotenv.config();
+    pool = mysql.createPool({
+        connectionLimit: 10,
+        host: 'db', // Cloud SQL Proxy コンテナのサービス名
+        port: 3306, // Cloud SQL Proxy がリッスンしているポート
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
+    });
+}else{
+    pool = mysql.createPool({
+        user: process.env.DB_USER, // e.g. 'my-db-user'
+        password: process.env.DB_PASS, // e.g. 'my-db-password'
+        database: process.env.DB_NAME, // e.g. 'my-database'
+        socketPath: process.env.INSTANCE_UNIX_SOCKET, // e.g. '/cloudsql/project:region:instance'
+    });
+}
+
+/** min以上max以下の整数値の乱数を返す */
+function intRandom(min, max){
+    return Math.floor( Math.random() * (max - min + 1)) + min;
+}
 
 
 app.post('/verifyToken', async (req, res) => {
@@ -111,68 +136,70 @@ app.get('/lesson-test', (req, res) => {
 
         let image_data;
         // .txt ファイルだけをフィルタリング
-        const textFiles = files.filter(file => path.extname(file) === '.text');
+        // const textFiles = files.filter(file => path.extname(file) === '.text');
 
-        // ランダムな .txt ファイルを選ぶ
-        const randomFile = textFiles[Math.floor(Math.random() * textFiles.length)];
+        
 
+        try {
+                
+                // クエリを実行
+                pool.query("SELECT * FROM question where lesson = ?", [lesson], (err, results) => {
+                    if (err) throw err;
+                    // console.log(results);
+                    // ランダムな .txt ファイルを選ぶ
+                    /** 重複チェック用配列 */
+                    var randoms = [];
+                    /** 最小値と最大値 */
+                    var min = 0, max = results.length;
+                    
+                    /** 重複チェックしながら乱数作成 */
+                    for(i = min; i <= 3; i++){
+                        while(true){
+                            var tmp = intRandom(min, max);
+                            if(!randoms.includes(tmp)){
+                                randoms.push(tmp);
+                                break;
+                            }
+                        }
+                    }
+                    const answer = results[randoms[0]];
+                    const wronge1 = results[randoms[1]];
+                    const wronge2 = results[randoms[2]];
+                    const wronge3 = results[randoms[3]];
+                    console.log(answer);
+                    console.log(wronge1);
+                    console.log(wronge2);
+                    console.log(wronge3);
+                    // ファイルの内容を読み取る
+                    fs.readFile(path.join('./b64_data', answer.image), 'utf8', (err, data) => {
+                        if (err) {
+                            res.status(500).send('Internal Server Error 2');
+                            return;
+                        }
+                        image_data = data;
 
-        // ファイルの内容を読み取る
+                        ans = answer.word;
+                        item_list = [answer.word, wronge1.word, wronge2.word, wronge3.word] // シャッフル
 
-        fs.readFile(path.join('./b64_data', randomFile), 'utf8', (err, data) => {
-            if (err) {
-                res.status(500).send('Internal Server Error 2');
-                return;
+                        // console.log(image_data);
+                        // base64形式で返す
+                        res.header('Access-Control-Allow-Origin', '*');
+                        res.json({
+                            "id": 1,
+                            "ans":ans,
+                            "item_list":item_list,
+                            "image": "data:image/png;base64," + image_data,
+                        });
+                    });
+                });
+            } catch (err) {
+                console.error('データベース操作エラー:', err);
+                res.status(500).send('データベース操作エラー');
             }
-            image_data = data;
-
-            // ans = 'apple';
-            // item_list = ['apple', 'banana', 'peach', 'grape'] // シャッフル
-
-            // console.log(image_data);
-            // base64形式で返す
-            res.header('Access-Control-Allow-Origin', '*');
-            res.json({
-                "id": 1,
-                "answer": "hogehoge",
-                "wronge1": "apple",
-                "wronge2": "apple",
-                "wronge3": "apple",
-                "image": "data:image/png;base64," + image_data,
-            });
-        });
+        
     });
 });
 
-// mysqlからデータを取得する
-// *****************
-// PostgreSQL connect info
-// const mysql = require('promise-mysql');
-// const dbConfig = {
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     socketPath: process.env.INSTANCE_UNIX_SOCKET,
-// };
-
-const createUnixSocketPool =  () => {
-    try {
-        // Note: Saving credentials in environment variables is convenient, but not
-        // secure - consider a more secure solution such as
-        // Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-        // keep secrets safe.
-        const pool = mysql.createPool({
-            user: process.env.DB_USER, // e.g. 'my-db-user'
-            password: process.env.DB_PASS, // e.g. 'my-db-password'
-            database: process.env.DB_NAME, // e.g. 'my-database'
-            socketPath: process.env.INSTANCE_UNIX_SOCKET, // e.g. '/cloudsql/project:region:instance'
-        });
-        console.log(pool);
-        return pool;
-    } catch (err) {
-        throw err;
-    }
-};
 
 // アプリ開始時に確認する.
 const readline = require('readline');
@@ -231,47 +258,39 @@ app.get('/addsql', async (req, res) => {
                     // console.log(textFiles)
                     Object.keys(namelist).forEach(function (word) {
                         textFiles.forEach(function (value) {
-                            if (!value.indexOf(namelist[word])) {
-                                
+                            if (!value.indexOf(namelist[word])) {                  
                                 // 文章と絵が入っているファイルを結びつけることができた。
-                                fs.readFile(path.join('./b64_data', value), 'utf8', (err, data) => {
-                                    if (err) {
-                                        res.status(500).send('Internal Server Error 2');
-                                        return;
-                                    }
-                                    image_data = data;
-                                    /**
-                                    *  table
-                                    * ___________________________
-                                    * id | word | image | lesson |
-                                    * ---------------------------
-                                    */
-                                    // console.log(word,lesson,"data:image/png;base64," + image_data);
-                                    try {
-                                        // Cloud SQL データベースに接続
-                                        const pool = createUnixSocketPool();
                                 
-                                        // クエリを実行
-                                        const results = pool.query(
-                                            'INSERT INTO question(word,image,lesson) VALUES(?,?,?)', 
-                                            [word,"data:image/png;base64," + image_data,lesson],
-                                            function(error, response) {
-                                                if(error) throw error;
-                                                    console.log(response);
+                                /**
+                                *  table
+                                * ___________________________
+                                * id | word | image | lesson |
+                                * ---------------------------
+                                */
+                                // console.log(word,lesson,"data:image/png;base64," + image_data);
+                                
+                                let flag;
+                                try {
+                                    dotenv.config();
+                                    // クエリを実行
+                                    pool.query("SELECT * FROM question where lesson = ?", [lesson], (err, database) => {
+                                        database.forEach(function(each_data){
+                                            if (each_data.image === value){
+                                                flag = 'dup'
                                             }
-                                        );
-                                        // プールを閉じる
-                                        pool.end();
-
-                                
-                                        // // クエリの結果をレスポンスとして返す
-                                        // res.json(results);
-                                    } catch (err) {
-                                        console.error('データベース操作エラー:', err);
-                                        res.status(500).send('データベース操作エラー');
-                                    }
-
-                                });
+                                        });
+                                        if (!(flag === 'dup')){
+                                            pool.query('INSERT IGNORE INTO question(word,image,lesson) VALUES(?,?,?)',[word,value,lesson],function(error, response) {
+                                                if(error) throw error;
+                                                    console.log(word,value,lesson);
+                                            });
+                                        }
+                                    });
+                                } catch (err) {
+                                    console.error('データベース操作エラー:', err);
+                                    res.status(500).send('データベース操作エラー');
+                                }
+  
                             }
                         });
                     });
@@ -291,92 +310,22 @@ app.get('/addsql', async (req, res) => {
     });
 });
 
-// (async () => {
-//     const connection = await mysql.createConnection({
-//         user: process.env.DB_USER, // e.g. 'my-db-user'
-//         password: process.env.DB_PASS, // e.g. 'my-db-password'
-//         database: process.env.DB_NAME, // e.g. 'my-database'
-//         socketPath: process.env.INSTANCE_UNIX_SOCKET, // e.g. '/cloudsql/project:region:instance'
-//     });
-
-//     const rows = await connection.query(`SELECT * FROM qquestion;`);
-//     console.log(rows);
-//     connection.end();
-// })();
-const mysql = require('mysql');
-
 
 
 // ルートハンドラーの定義
 app.get('/mysql',  (req, res) => {
-
-    if (process.env.NODE_ENV !== "production") { // production is 本番環境
+    try {
         dotenv.config();
-        const pool = mysql.createPool({
-            connectionLimit: 10,
-            host: 'db', // Cloud SQL Proxy コンテナのサービス名
-            port: 3306, // Cloud SQL Proxy がリッスンしているポート
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
-            database: process.env.DB_NAME
-        });
             // 例: クエリの実行
         pool.query('SELECT * FROM question', (err, results) => {
             if (err) throw err;
             console.log(results);
         });
         console.log("development");
-    } else {
-        const pool = mysql.createPool({
-            user: process.env.DB_USER, // e.g. 'my-db-user'
-            password: process.env.DB_PASS, // e.g. 'my-db-password'
-            database: process.env.DB_NAME, // e.g. 'my-database'
-            socketPath: process.env.INSTANCE_UNIX_SOCKET, // e.g. '/cloudsql/project:region:instance'
-        });
-        pool.query('SELECT * FROM question', (err, results) => {
-            if (err) throw err;
-            console.log(results);
-        });
-        console.log("development");
-        console.log("production");
+    } catch (err) {
+        console.error('データベース操作エラー:', err);
+        res.status(500).send('データベース操作エラー');
     }
-    
-    
-    // try {
-    //     const pool = mysql.createPool({
-    //         user: process.env.DB_USER, // e.g. 'my-db-user'
-    //         password: process.env.DB_PASS, // e.g. 'my-db-password'
-    //         database: process.env.DB_NAME, // e.g. 'my-database'
-    //         socketPath: process.env.INSTANCE_UNIX_SOCKET, // e.g. '/cloudsql/project:region:instance'
-    //     });
-
-    //     // // Cloud SQL データベースに接続
-    //     // const pool = await createUnixSocketPool();
-
-    //     pool.getConnection()
-    //         .then(connection => {
-    //             return connection.query('SELECT * FROM question')
-    //             .then(results => {
-    //                 console.log(results);
-    //                 connection.release();  // 接続をプールに戻す
-    //             })
-    //             .catch(error => {
-    //                 console.error('Error executing query:', error);
-    //             });
-    //         })
-    //         .catch(error => {
-    //             console.error('Error getting connection:', error);
-    //         });
-
-    //     // プールを閉じる
-    //     pool.end();
-
-    //     // クエリの結果をレスポンスとして返す
-    //     res.json(results);
-    // } catch (err) {
-    //     console.error('データベース操作エラー:', err);
-    //     res.status(500).send('データベース操作エラー');
-    // }
 });
 
 app.listen(PORT, () => {
